417ae9fc5f646f3bda8aa59e2af2de9f85ddb4ed
RasmusOlsen0 <raol0001@stud.kea.dk>
2024-02-15 19:05:53 +0100

    svamp siden

--- svamp.js
+++ svamp.js
@@ -13,4 +13,16 @@ fetch(`https://auovcezakqpcoioboayh.supabase.co/rest/v1/mushrooms?id=eq.${id}`, 
 function singleSvamp(enkeltSvamp) {
   const svamp = enkeltSvamp[0];
   console.log(svamp);
+
+  document.querySelector("h1").textContent = svamp.name;
+  document.querySelector(".season").textContent = svamp.season;
+  document.querySelector(".description").textContent = svamp.description;
+  document.querySelector(".location_description").textContent = svamp.location_description;
+  document.querySelector(".location").textContent = svamp.location;
+  document.querySelector(".months").textContent = svamp.months;
+  document.querySelector(".sankning").textContent = svamp.sankning;
+  document.querySelector(".handling").textContent = svamp.handling;
+  document.querySelector(".application").textContent = svamp.application;
+  document.querySelector(".storage").textContent = svamp.storage;
+  document.querySelector(".replacement").textContent = svamp.replacement;
 }
